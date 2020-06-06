02d2d0479d5e0178ac67f25c577c6ed80f460cdd
Vinayak Patil <vinayak.p@unibrain.in>
2020-06-06 13:20:06 +0530

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
+import About from "../components/About"
+import Tips from "../components/Tips"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+      <Layout>
+        <SimpleHero>
+          <Banner title="Awesome Hampi" info="Come and explore Hampi, the city of ruins">
+            <Link to="/places" className="btn-white">explore places</Link>
+          </Banner>
+        </SimpleHero>
+        <About />
+        <Tips />
+      </Layout>
+  )
 }
